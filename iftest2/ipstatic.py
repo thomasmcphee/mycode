ef1ec826c3ec31d53b50917b8f2ceea104b367d2
Thomas McPhee <thomasmcphee@users.noreply.github.com>
2022-03-08 20:15:26 +0000

    working on if, elif, else logic

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Alta3 Research | RZFeeser
+    conditionals - strings test true"""
+
+ipchk = "192.168.0.1"
+
+if ipchk:
+    print("Looks like the IP address was set: " + ipchk)
