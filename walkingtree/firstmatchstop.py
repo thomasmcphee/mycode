0df0a90d41103618f15f8e79e457e788f0524199
Thomas McPhee <thomasmcphee@users.noreply.github.com>
2022-03-10 20:25:00 +0000

    python and excel, including working with pandas

---
+++ walkingtree/firstmatchstop.py
@@ -0,0 +1,10 @@
+import os
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+lookfor = input("What am I looking for? ")
+loowhere = input("What is the path in which I should search? ")
+
+print(find(lookfor, lookwhere))
